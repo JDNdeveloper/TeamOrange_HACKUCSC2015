f9a23d04dc54e716a2bce8ea00bcdb219f251878
Jayden Navarro <jdndeveloper@gmail.com>
2015-01-10 13:13:23 -0800

    Made ImageButton class better

--- NewSquareGame/core/src/com/TeamOrange/NewSquareGame/ImageButton.java
+++ NewSquareGame/core/src/com/TeamOrange/NewSquareGame/ImageButton.java
@@ -1,30 +1,41 @@
 package com.TeamOrange.NewSquareGame;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by Jayden Navarro on 1/10/2015.
  */
 public class ImageButton {
+    Texture image;
+    Sprite imageSprite;
+
     float mouseX;
     float mouseY;
 
-    float topLeftX;
-    float topLeftY;
-    float bottomRightX;
-    float bottomRightY;
+    float imageX;
+    float imageY;
 
-    public ImageButton(int tlX, int tlY, int brX, int brY) {
-        topLeftX = tlX;
-        topLeftY = tlY;
-        bottomRightX = brX;
-        bottomRightY = brY;
+    public ImageButton(String imageName, int iX, int iY) {
+
+        imageX = iX;
+        imageY = iY;
+
+        image = new Texture(imageName);
+        imageSprite = new Sprite(image);
+        imageSprite.setPosition(imageX, imageY);
     }
 
     public boolean mouseWithinRegion(int X, int Y) {
         mouseX = X;
         mouseY = Y;
 
-        if (!(mouseX < bottomRightX && mouseX > topLeftX)) return false; //if x not in bounds
-        if (!(mouseY < topLeftY && mouseY > bottomRightY)) return false;
+        float topRightX = image.getWidth() + imageX;
+        float topRightY = image.getHeight() + imageY;
+
+
+        if (!(mouseX < topRightX && mouseX > imageX)) return false; //if x not in bounds
+        if (!(mouseY < topRightY && mouseY > imageY)) return false; //if y not in bounds
 
         return true;
     }
