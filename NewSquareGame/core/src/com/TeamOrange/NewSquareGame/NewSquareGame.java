bc87749a6dd38c15ec7882b70f2da3f1336cf7ba
Jayden Navarro <jdndeveloper@gmail.com>
2015-01-10 11:45:53 -0800

    Added movement to square

--- NewSquareGame/core/src/com/TeamOrange/NewSquareGame/NewSquareGame.java
+++ NewSquareGame/core/src/com/TeamOrange/NewSquareGame/NewSquareGame.java
@@ -2,18 +2,21 @@ package com.TeamOrange.NewSquareGame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class NewSquareGame extends ApplicationAdapter {
+public class NewSquareGame extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
-	
+	int timer = 0;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("square.png");
+        Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -21,7 +24,48 @@ public class NewSquareGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.9f, 0.9f, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, timer + Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
 		batch.end();
 	}
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        timer += 10;
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
