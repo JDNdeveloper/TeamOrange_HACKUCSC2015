a157290f1bea7b128785ebb9b29fbfdabcec32a7
Brad <bqm111@gmail.com>
2015-01-10 20:32:03 -0800

    star

--- NewSquareGame/core/src/com/TeamOrange/NewSquareGame/Star.java
+++ NewSquareGame/core/src/com/TeamOrange/NewSquareGame/Star.java
@@ -1,7 +1,20 @@
 package com.TeamOrange.NewSquareGame;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by Brad on 1/10/2015.
  */
 public class Star {
+    public Star(float x, float y){
+
+    }
+
+    public void act(){
+
+    }
+
+    public void draw(SpriteBatch batch){
+        
+    }
 }
