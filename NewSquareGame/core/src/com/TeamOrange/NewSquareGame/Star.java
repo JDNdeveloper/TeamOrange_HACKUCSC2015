7bd229ac331c39ed51213550e035d6b5705412d7
Brad <bqm111@gmail.com>
2015-01-10 22:14:13 -0800

    img

--- NewSquareGame/core/src/com/TeamOrange/NewSquareGame/Star.java
+++ NewSquareGame/core/src/com/TeamOrange/NewSquareGame/Star.java
@@ -19,12 +19,50 @@ public class Star {
     Texture texture;
     Sprite sprite;
     Body body;
-    Vector2 bottomLeftCorner;
-    float scale = 1;
+    float scale = 1f;
     boolean scaleFlip = false;
-    float rotation = 0;
+    float rotation = 0f;
+    Particle particles[] = new Particle[50];
 
+    private class Particle {
+        Texture texture;
+        Sprite sprite;
+        Body body;
+        float scale = 1f;
+
+        public Particle(float x, float y, World world){
+            texture = new Texture("particle.png");
+            sprite = new Sprite(texture);
+            sprite.setPosition(x, y);
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+            bodyDef.position.set((x + sprite.getWidth() /2) / Constants.PIXELS_TO_METERS, (y + sprite.getHeight() / 2) / Constants.PIXELS_TO_METERS);
+
+            body = world.createBody(bodyDef);
+
+            CircleShape shape = new CircleShape();
+            shape.setRadius(sprite.getWidth() / 2 / Constants.PIXELS_TO_METERS);
+
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            fixtureDef.density = 0.1f;
+
+            body.createFixture(fixtureDef);
+            shape.dispose();
+
+            body.applyForceToCenter((float) Math.random(),(float) Math.random(),true);
+        }
+
+        public float getScale(){
+            return scale;
+        }
+
+        public void setScale(float newScale){
+            scale = newScale;
+        }
+    }
     public Star(float x, float y, World world){
+
             texture = new Texture("star.png");
             sprite = new Sprite(texture);
             sprite.setPosition(x, y);
@@ -44,6 +82,9 @@ public class Star {
             body.createFixture(fixtureDef);
             shape.dispose();
 
+            for(int i =0;i<particles.length;i++) {
+                particles[i] = new Particle((float) (x + Math.random() * sprite.getWidth()), y, world);
+            }
     }
 
     public void act(){
@@ -56,6 +97,10 @@ public class Star {
             scale += 0.01f;
         }if(scale>=1f){
             scaleFlip = false;
+        }
+        for(int i=0;i<particles.length;i++){
+            particles[i].setScale(particles[i].getScale());
+
         }
     }
 
