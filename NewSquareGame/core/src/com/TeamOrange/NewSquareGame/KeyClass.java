bbb3613ec19e4f853ad0a76e707119a328b55cd0
John Gemignani <jrgemigna@ucsc.edu>
2015-01-10 18:45:41 -0800

    checkBounds
    adjust bounds

--- NewSquareGame/core/src/com/TeamOrange/NewSquareGame/KeyClass.java
+++ NewSquareGame/core/src/com/TeamOrange/NewSquareGame/KeyClass.java
@@ -24,7 +24,7 @@ public class KeyClass {
         Transform bodyPosition = body.getTransform();
         Vector2 test = bodyPosition.getPosition();
 
-        if (test.y < 0 || test.x < 0 || test.x > Gdx.graphics.getWidth()/PIXELS_TO_METERS) {
+        if (test.y < 0 || test.x < 0 || test.x > screenWidthMeters) {
             body.setLinearVelocity(0f, 0f);
             body.setAngularVelocity(0f);
             body.setTransform(screenCenter(), 0f);
