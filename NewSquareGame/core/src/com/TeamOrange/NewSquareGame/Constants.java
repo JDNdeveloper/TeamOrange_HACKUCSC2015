c0bd95a5ea3eda49a6ad3097bdeedc42cb9aa4f8
John Gemignani <jrgemigna@ucsc.edu>
2015-01-10 18:50:06 -0800

    Constants
    file of static constants

---
+++ NewSquareGame/core/src/com/TeamOrange/NewSquareGame/Constants.java
@@ -0,0 +1,18 @@
+package com.TeamOrange.NewSquareGame;
+
+/**
+ * Created by jgemig on 1/10/2015.
+ */
+public class Constants {
+
+    static final float GRAVITY = -6.0f;
+    static final float JUMPFORCE = 10.0f;
+
+    static final float buttonPaddingX = 32;
+    static final float buttonPaddingY = 32;
+    static final float diagonalButtonOffset = 96;
+
+    static float torque = 0.0f;
+
+    static final float PIXELS_TO_METERS = 100f;
+}
